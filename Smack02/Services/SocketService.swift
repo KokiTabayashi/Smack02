44c6cd25697097cf3d49c0c06373cbe8dbbd5f4f
Koki Tabayashi <koki@K-MacBook-Pro.local>
2018-04-21 15:21:18 -0700

    lec 84

---
+++ Smack02/Services/SocketService.swift
@@ -0,0 +1,48 @@
+//
+//  SocketService.swift
+//  Smack02
+//
+//  Created by Koki Tabayashi on 2018/04/21.
+//  Copyright © 2018年 Koki Tabayashi. All rights reserved.
+//
+
+import UIKit
+import SocketIO
+
+class SocketService: NSObject {
+
+    static let instance = SocketService()
+    
+    override init() {
+        super.init()
+    }
+    
+//    var socket : SocketIOClient = SocketIOClient(socketURL: URL(string: BASE_URL)!)
+    let manager = SocketManager(socketURL: URL(string: BASE_URL)!)
+    lazy var socket: SocketIOClient = manager.defaultSocket
+    
+    func establishConnection() {
+        socket.connect()
+    }
+    
+    func closeConnection() {
+        socket.disconnect()
+    }
+    
+    func addChannel(channelName: String, channelDescription: String, completion: @escaping CompletionHandler) {
+        socket.emit("newChannel", channelName, channelDescription)
+        completion(true)
+    }
+    
+    func getChannel(completion: @escaping CompletionHandler) {
+        socket.on("channelCreated") { (dataArray, ack) in
+            guard let channelName = dataArray[0] as? String else { return }
+            guard let channelDesc = dataArray[1] as? String else { return }
+            guard let channelId = dataArray[2] as? String else { return }
+            
+            let newChannel = Channel(channelTitle: channelName, channelDescription: channelDesc, id: channelId)
+            MessageService.instance.channels.append(newChannel)
+            completion(true)
+        }
+    }
+}
